3abe3b9db2665d36c90869471eae0a42b9c18917
u1054333 <u1054333@eng.utah.edu>
2021-09-23 12:59:35 -0600

    Brief update of the classes I intend to use to store useful data from the test and training examples, need further refinement and additions

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,3 +1,5 @@
+#import the-things-I-need
+
 class Example:
     example_number
     attributes
@@ -10,12 +12,32 @@ class Example:
 
 class Attribute:
     attribute_name
-    attribute_value
+    num_each_value
+    attribute_value_counts
+    #attribute_values_neg
+
+    def __init__(self, attr_name):
+        self.attribute_name = attr_name          
+        self.num_each_value = { }
+        self.attribute_value_counts = { }   #make into a default dictionary, each key is attribute value, goes to array 0 <-> labels-1
+     #   self.attribute_values_neg = { }   #make into a default dictionary (correct?)
+
     
 class Label_data:
+    total_num_labels
     label_values_and_counts
+
+    def __init__(self, total_num, labels_and_values):
+        self.total_num_values = total_num
+        self.label_values_and_counts = labels_and_values
 
 class Attribute_Data:
     attribute_name
     attribute_values
     value_counts
+
+
+#Beginning of file that does the work I need
+
+
+id = 1

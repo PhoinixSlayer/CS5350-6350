d609dd40f15b57e678e18f5104f02c34c90472cb
u1054333 <u1054333@eng.utah.edu>
2021-10-17 15:43:25 -0600

    Made simple formatting changes to make code look a little nicer as well as delete and change comments to look better and describe ideas better so it looks less cluttered.

--- DecisionTree_Main/DecisionTree_Main/DecisionTree_Main.py
+++ DecisionTree_Main/DecisionTree_Main/DecisionTree_Main.py
@@ -1,36 +1,23 @@
-# File for main DecisionTree project
-#import the-things-I-need
+# File for DecisionTree project
 import math as m
 import pandas as pd
 
 class Example:
-    #example_number = -1
-    #attributes     = []
-    #label          = ""
     def __init__(self, ex_number, attributes, lbl):
         self.example_number = ex_number
         self.attributes = attributes
         self.label = lbl
 
 class AttributeData:
-    #attribute_name          = ""
-    #num_each_value          = {}
-    #attribute_value_counts  = {}
-    #attribute_values_neg
     def __init__(self, attr_name):
         self.attribute_name = attr_name
         self.num_each_value = { }
-        self.value_label_counts = { }   #make into a default dictionary, each key is attribute value, goes to array 0 <-> labels-1
-     #   self.attribute_values_neg = { }   #make into a default dictionary (correct?)
+        self.value_label_counts = { }
 
-    
 class Label_data:
-    #total_num_labels        = 0 # equivalent to saying total number of examples
-    #label_values_and_counts = {}
     def __init__(self, total_num, labels_and_values):
-        self.total_num_values = total_num
-        self.label_values_and_counts = labels_and_values
-
+        self.total_num_values = total_num  # Represents number of examples tangentially
+        self.label_values_and_counts = labels_and_values  # Represents the totals for each label in a dictionary
 
 # Class for the decision tree representation of the data
 class Node:
@@ -38,7 +25,6 @@ class Node:
         self.Prediction = None  # If a leaf, the label that this path would predict for the example
         self.Value_of_Parent_Attribute = None  # The attribute that the parent node of this child split on
         self.Attribute_Split_With = ""  # The attribute that this specific node split on (if applicable)
-        self.Value = ""  # Based on the attribute this nodes parent split on, this value refers to the subset of the parent's attribute group
         self.Examples_in_Branch = {}  # The examples that we can use in this section of the tree
         self.Labels_in_Branch = None  # The label data for the above examples
         self.Remaining_Attributes = []  # The attributes that we haven't used in this section of the tree
@@ -47,13 +33,12 @@ class Node:
 
 
 
-# Method for calculating gain when labels are binary, but don't think this will every be needed considering mult version does the same thing
+# Method for calculating gain when labels are binary
 def InfoGain(positive_examples, negative_examples, total_examples):
     pos = positive_examples/total_examples
     neg = negative_examples/total_examples
     return -(pos*m.log2(pos))-(neg*m.log2(neg))
 
-# Pretty sure this is correct, but need to double check
 def MajorityError(majority_examples, total_examples):
     # Some att-values with have no examples to their subgroup, which means in the gains calculation its ME=0 times the proportion of this value
     #  among the total examples in that subset
@@ -61,16 +46,13 @@ def MajorityError(majority_examples, total_examples):
         return 0
     return 1 - majority_examples/total_examples
     
-# Method for calculating GI when possible labels are binary, but I don't think this will be needed.
+# Method for calculating GI when possible labels are binary
 def GiniIndex(positive_examples, negative_examples, total_examples):
     pos = positive_examples/total_examples
     neg = negative_examples/total_examples
     return 1 - (pos**2 + neg**2)
 
 ## These are the versions of the above that calculate the info gain for a given attribute based on multiple label values.
-# Based on my foggy memory, this is what the entropy version should look like for some number of possible labels. The idea is
-#  that if there are multiple labels an example can have, the total entropy for an attribute value is the combination of each of the 
-#  different label quantities each divided by the total number of examples in this attribute value.
 def GiniIndexMult(label_quantities, total_examples):
     total = 0.0
     for labels in label_quantities:
@@ -92,7 +74,9 @@ def InfoGainMult(label_quantities, total_examples):
     return total
 
 
-# Method that constructs the tree recursively
+
+## Method that constructs the tree recursively
+# Doesn't make the most accurate tree as of right now, but it's at least something to start with
 def SplitNode(sec_root, version, current_level, desired_level):
     # --- Base Cases ---
     for l in sec_root.Labels_in_Branch.label_values_and_counts:
@@ -119,6 +103,7 @@ def SplitNode(sec_root, version, current_level, desired_level):
         sec_root.Prediction = current_best
         return
 
+
     labels = []
     for l in sec_root.Labels_in_Branch.label_values_and_counts:
         labels.append(sec_root.Labels_in_Branch.label_values_and_counts[l])
@@ -133,7 +118,7 @@ def SplitNode(sec_root, version, current_level, desired_level):
     if version == 2: # Here we use the Gini Index
         overall_gain = GiniIndexMult(labels, sec_root.Labels_in_Branch.total_num_values)
 
-    Atts_data = {}  # Data for each of the attributes we can work with
+    Atts_data = {}
     for a in sec_root.Remaining_Attributes: # for this attribute ...
         temp = AttributeData(a)
         for v in car_attribute_values[a]: # for each of the attributes values ...
@@ -143,7 +128,7 @@ def SplitNode(sec_root, version, current_level, desired_level):
                 temp.value_label_counts[v].update({l: 0})
         Atts_data.update({a:temp})
     
-    # with both of the dictionaries prepared, we can now fill them with the appropriate attribute data
+    # With both of the dictionaries prepared, we can now fill them with the appropriate attribute data
     for e in sec_root.Examples_in_Branch:
         example = sec_root.Examples_in_Branch[e]
         for a in sec_root.Remaining_Attributes:
@@ -151,7 +136,6 @@ def SplitNode(sec_root, version, current_level, desired_level):
             value = example.attributes[a]
             data.num_each_value[value] += 1
             data.value_label_counts[value][example.label] += 1
-    # After this point, all of the data should be accurately totaled so we can calculate the desired info
 
     # After each of the attributes have been built and the data correctly parsed, we can then do our calculations to determine which one is
     #  best to split on at this stage of the tree
@@ -159,7 +143,6 @@ def SplitNode(sec_root, version, current_level, desired_level):
     for a in sec_root.Remaining_Attributes:
         gains.update({a: 0})
 
-    # Need to add option of calculation method here, so that each of the three IG, ME, and GI can be used depending on which is desired
     for a in sec_root.Remaining_Attributes:
         value_total = 0
         current_attr = Atts_data[a]
@@ -187,7 +170,6 @@ def SplitNode(sec_root, version, current_level, desired_level):
                 else:
                     value_sum = GiniIndexMult(label_total_list, total_for_value)
                     value_total += value_sum * (total_for_value / sec_root.Labels_in_Branch.total_num_values)
-        # Pretty sure that with each of the values of the attribute determined, its just the overall gain minus this total
         gains[a] = overall_gain - value_total
 
     # Determine the best attribute to split on based on gathered data
@@ -205,7 +187,6 @@ def SplitNode(sec_root, version, current_level, desired_level):
     for v in attr_set.num_each_value:
         new = Node()
         new.Value_of_Parent_Attribute = a
-        # new.Value = v  ## this is probably unneeded since we are accessing it with the att-value via the dictionary
         new.parent = sec_root
         new.Remaining_Attributes = sec_root.Remaining_Attributes[:]
         new.Remaining_Attributes.remove(a)
@@ -217,7 +198,6 @@ def SplitNode(sec_root, version, current_level, desired_level):
         sec_root.node_list.update({v: new})
         
     for example in sec_root.Examples_in_Branch.values():
-        #ex_value = example.attributes[attribute]
         valued_node = sec_root.node_list[example.attributes[a]]
         valued_node.Examples_in_Branch.update({example.example_number: example})
         valued_node.Labels_in_Branch.total_num_values += 1
@@ -226,12 +206,8 @@ def SplitNode(sec_root, version, current_level, desired_level):
     # With nodes created and added to the tree, all that is left is to run this work on those to further expand or complete the tree
     for n in sec_root.node_list.values():
         SplitNode(n, version, current_level+1, desired_level)
-    # I don't think there is any more work after this point, all that is needed now is adding ability for method to build tree to certain
-    #  heights, make it so that the desired method of calculation is used based on the version variable, and that the tree works correctly
     # Later will need to figure out how to make it so the tree can take certain variables and crunch them into binary variables, but I think
     #  that that kind of work would happen in an earlier part of the algorithm.
-    # I think I am probably missing a few things, but I won't be able to make sure until later after I've rested a bit and can look over
-    #  everything with a fresh start, ready to (mostly) finish this part and hopefully start moving on by preparing something for HW2
 
 
 
@@ -245,6 +221,7 @@ def DetermineTree(all_examples, total_labels, calculation_version, desired_level
 
     SplitNode(root, calculation_version, 0, desired_level)
     return root
+
 
 
 ## Method used to test tree on all examples and gather data for accuracy comparisons
@@ -287,12 +264,11 @@ car_attribute_values = { "buying": {"vhigh", "high", "med", "low"},
                     "lug_boot": {"small", "med", "big"},
                    "safety": {"low", "med", "high"} }
 
+bank_attribute_values = {}
+
 def main():
     car_labels = Label_data(0, { "unacc": 0, "acc": 0, "good": 0, "vgood": 0 })
     car_labels_test = Label_data(0, { "unacc": 0, "acc": 0, "good": 0, "vgood": 0 })
-
-    #car_attributes_data  = ["buying", "maint", "doors", "persons", "lug_boot", "safety"]
-    #bank_attributes = [ ]
 
     train_examples = {}
     test_examples = {}
@@ -314,10 +290,6 @@ def main():
     # --- Request here ---
     requested_level = 0;
 
-    # Upon completion of data extraction, calculate a decision tree for each method of info gain using the data
-    # When it comes to running tree up to certain levels, I could handle it as a console read based on what the person wants, but it
-    #  seems like it would be simpler to have the program make trees of multiple levels anyways because they want a tree of every level
-    #  possible.
     GainRoot = DetermineTree(train_examples, car_labels, 0, 6, ["buying", "maint", "doors", "persons", "lug_boot", "safety"])
     MERoot = DetermineTree(train_examples, car_labels, 1, 6, ["buying", "maint", "doors", "persons", "lug_boot", "safety"])
     GiniRoot = DetermineTree(train_examples, car_labels, 2, 6, ["buying", "maint", "doors", "persons", "lug_boot", "safety"])
@@ -338,6 +310,8 @@ def main():
             car_labels_test.total_num_values += 1
             car_labels_test.label_values_and_counts[temp.label] += 1
             id += 1
+
+    # Might try making trees from the test data as well and seeing how the testing compares, but if I do it will have to be later.
 
     TestTree(GainRoot, test_examples)
     TestTree(MERoot, test_examples)
@@ -360,7 +334,6 @@ def main():
         for l in f:
             terms = l.strip().split(',')
             id += 1
-
     
 
 

ebf78691231769ac2ef9c791fde44cbec7ea8206
u1054333 <u1054333@eng.utah.edu>
2021-11-17 18:27:43 -0700

    Minor changes, added a helper method(s).  Some minor tweaks to catch errors, added code that calculates the varied weight vectors with the second Schedule method.  Just need to set up prints and add commentary for comparison in the coding portion of the assignment.

--- SVM/SVM/SVM/SVM.py
+++ SVM/SVM/SVM/SVM.py
@@ -48,8 +48,17 @@ def Schedule2(g, t):
     return (g/(1 + t))
 
 
+def CompSign(res):
+    sign = 0
+    if res >= 0:
+        sign = 1
+    else:
+        sign = -1
+    return sign
+
+
 def SVM_Stochastic(train, test, T):
-    w1, w2, w3 = [0,0,0,0,0]
+    w1, w2, w3, ww1, ww2, ww3 = [0,0,0,0,0]
     N = len(train)
     gamma = 1  # In this algorithm this is the learning rate
     curr_gamma = 1
@@ -76,7 +85,7 @@ def SVM_Stochastic(train, test, T):
                 w2 = Update(w2, curr_gamma, C[1], N, example)
             else:
                 for j in len(w2):
-                    w1[j] = (1-curr_gamma) * w1[j]
+                    w2[j] = (1-curr_gamma) * w2[j]
 
             if pred3 <= 1:
                 w3 = Update(w3, curr_gamma, C[2], N, example)
@@ -92,74 +101,125 @@ def SVM_Stochastic(train, test, T):
             # epoch we set the main gamma to something less with a simple calculation, so this iterated gamma based on t is different
             # each epoch?
 
+    N = len(train)
+    gamma = 1  # In this algorithm this is the learning rate
+    curr_gamma = 1
+    t = 0
+    for i in range(T):
+        shuffled_order = r.sample( range(1, len(train)+1), len(train))
+        for index in shuffled_order:
+            example = train[index]
+            pred1 = VectorMult(ww1, example)
+            pred2 = VectorMult(ww2, example)
+            pred3 = VectorMult(ww3, example)
+            if pred1 <= 1:
+                ww1 = Update(ww1, curr_gamma, C[0], N, example)
+            else:
+                for j in len(ww1):
+                    ww1[j] = (1-curr_gamma) * ww1[j]
+
+            if pred2 <= 1:
+                ww2 = Update(ww2, curr_gamma, C[1], N, example)
+            else:
+                for j in len(ww2):
+                    ww2[j] = (1-curr_gamma) * ww2[j]
+
+            if pred3 <= 1:
+                ww3 = Update(ww3, curr_gamma, C[2], N, example)
+            else:
+                for j in len(ww3):
+                    ww3[j] = (1-curr_gamma) * ww3[j]
+
+            t += 1 # Do this here? or after?
+            curr_gamma = Schedule2(gamma, t)
+
+
     # With the weight vectors complete, we can now run tests with them
     error1, error2, error3 = 0  # For now we'll simply tally incorrect predictions on the data
+    serr1, serr2, serr3 = 0
     for i in train:
         example = train[i]
         result = Test(w1, example)
-        sign = 0
-        if result >= 0:
-            sign = 1
-        else:
-            sign = -1
+        sign = CompSign(result)
         if sign != example.label:
             error1 += 1
 
         result = Test(w2, example)
-        sign = 0
-        if result >= 0:
-            sign = 1
-        else: 
-            sign = -1
+        sign = CompSign(result)
         if sign != example.label:
             error2 += 1
 
         result = Test(w3, example)
-        sign = 0
-        if result >= 0:
-            sign = 1
-        else:
-            sign = -1
+        sign = CompSign(result)
         if sign != example.label:
             error3 += 1
 
+        result = Test(ww1, example)
+        if CompSign(result) != example.label:
+            serr1 += 1
+
+        result = Test(ww2, example)
+        if CompSign(result) != example.label:
+            serr2 += 1
+
+        result = Test(ww3, example)
+        if CompSign(result) != example.label:
+            serr3 += 1
+
+    # Error values using the first Schedule method
     error1 = error1 / len(train)
     error2 = error2 / len(train)
     error3 = error3 / len(train)
 
+    # Error values for variants using the second Schedule method
+    serr1 = serr1 / len(train)
+    serr2 = serr2 / len(train)
+    serr3 = serr3 / len(train)
+
+    ## Need to add print statements and such later so that we can see the error rates between the different C's and data sets using Sched1
+
     terr1, terr2, terr3 = 0
+    sterr1, sterr2, sterr3 = 0
     for i in test:
         example = test[i]
         result = Test(w1, example)
-        sign = 0
-        if result >= 0:
-            sign = 1
-        else:
-            sign = -1
+        sign = CompSign(result)
         if sign != example.label:
             terr1 += 1
 
         result = Test(w2, example)
-        sign = 0
-        if result >= 0:
-            sign = 1
-        else: 
-            sign = -1
+        sign = CompSign(result)
         if sign != example.label:
             terr2 += 1
 
         result = Test(w3, example)
-        sign = 0
-        if result >= 0:
-            sign = 1
-        else:
-            sign = -1
+        sign = CompSign(result)
         if sign != example.label:
             terr3 += 1
+
+        result = Test(ww1, example)
+        if CompSign(result) != example.label:
+            sterr1 += 1
+
+        result = Test(ww2, example)
+        if CompSign(result) != example.label:
+            sterr2 += 1
+
+        result = Test(ww3, example)
+        if CompSign(result) != example.label:
+            sterr3 += 1
 
     terr1 = terr1 / len(test)
     terr2 = terr2 / len(test)
     terr3 = terr3 / len(test)
+
+    sterr1 = sterr1 / len(test)
+    sterr2 = sterr2 / len(test)
+    sterr3 = sterr3 / len(test)
+
+    ## Need to add print statements to do the same as above
+
+    ######  Need to add code to do the same thing, but this time with the second schedule described in the assignment
 
 
 def DualDomain(train, test, T):

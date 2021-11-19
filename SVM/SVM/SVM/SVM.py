a184669850611c954899f87256d7a2c8bae6b55e
u1054333 <u1054333@eng.utah.edu>
2021-11-18 17:32:18 -0700

    Initial test finished, outputted results seemingly without much trouble.  Results are a little less than favorable, but are something to work with for now.

--- SVM/SVM/SVM/SVM.py
+++ SVM/SVM/SVM/SVM.py
@@ -19,7 +19,7 @@ class Example:
 def Update(w, g, C, N, example): #! Need to change soon
     new_weight = [0] * len(w)
 
-    for i in len(w):
+    for i in range(len(w)):
         new_weight[i] = w[i] - g*w[i] + g * C * N * example.label * example.attributes[i]
 
     return new_weight
@@ -33,7 +33,7 @@ def VectorMult(weight, example):
     return total
 
 ##
-def Predict(weight, example):
+def Test(weight, example):
     total = 0
     for i in range(len(weight)):
         total += example.attributes[i] * weight[i]
@@ -58,7 +58,12 @@ def CompSign(res):
 
 
 def SVM_Stochastic(train, test, T):
-    w1, w2, w3, ww1, ww2, ww3 = [0,0,0,0,0]
+    w1 = [0,0,0,0,0]
+    w2 = [0,0,0,0,0]
+    w3 = [0,0,0,0,0]
+    ww1 = [0,0,0,0,0]
+    ww2 = [0,0,0,0,0]
+    ww3 = [0,0,0,0,0]
     N = len(train)
     gamma = 1  # In this algorithm this is the learning rate
     curr_gamma = 1
@@ -75,19 +80,19 @@ def SVM_Stochastic(train, test, T):
             if pred1 <= 1:
                 w1 = Update(w1, curr_gamma, C[0], N, example)
             else:
-                for j in len(w1):
+                for j in range(len(w1)):
                     w1[j] = (1-curr_gamma) * w1[j]
 
             if pred2 <= 1:
                 w2 = Update(w2, curr_gamma, C[1], N, example)
             else:
-                for j in len(w2):
+                for j in range(len(w2)):
                     w2[j] = (1-curr_gamma) * w2[j]
 
             if pred3 <= 1:
                 w3 = Update(w3, curr_gamma, C[2], N, example)
             else:
-                for j in len(w3):
+                for j in range(len(w3)):
                     w3[j] = (1-curr_gamma) * w3[j]
 
             t += 1 # Do this here? or after?
@@ -112,19 +117,19 @@ def SVM_Stochastic(train, test, T):
             if pred1 <= 1:
                 ww1 = Update(ww1, curr_gamma, C[0], N, example)
             else:
-                for j in len(ww1):
+                for j in range(len(ww1)):
                     ww1[j] = (1-curr_gamma) * ww1[j]
 
             if pred2 <= 1:
                 ww2 = Update(ww2, curr_gamma, C[1], N, example)
             else:
-                for j in len(ww2):
+                for j in range(len(ww2)):
                     ww2[j] = (1-curr_gamma) * ww2[j]
 
             if pred3 <= 1:
                 ww3 = Update(ww3, curr_gamma, C[2], N, example)
             else:
-                for j in len(ww3):
+                for j in range(len(ww3)):
                     ww3[j] = (1-curr_gamma) * ww3[j]
 
             t += 1 # Do this here? or after?
@@ -132,8 +137,12 @@ def SVM_Stochastic(train, test, T):
 
 
     # With the weight vectors complete, we can now run tests with them
-    error1, error2, error3 = 0  # For now we'll simply tally incorrect predictions on the data
-    serr1, serr2, serr3 = 0
+    error1 = 0
+    error2 = 0
+    error3 = 0  # For now we'll simply tally incorrect predictions on the data
+    serr1 = 0
+    serr2 = 0
+    serr3 = 0
     for i in train:
         example = train[i]
         result = Test(w1, example)
@@ -176,14 +185,18 @@ def SVM_Stochastic(train, test, T):
     print("The error rates on the train data using Schedule 1 are: ")
     print("For C = 100/873: " + str(error1) + ", for C = 500/873: " + str(error2) + ", for C = 700/873: " + str(error2))
 
-    print("") # Will this make a new line?
-
     print("The error rates on the train data using Schedule 2 are: ")
     print("For C = 100/873: " + str(serr1) + ", for C = 500/873: " + str(serr2) + ", for C = 700/873: " + str(serr3))
+
+    print("")
     ## Need to add print statements and such later so that we can see the error rates between the different C's and data sets using Sched1
 
-    terr1, terr2, terr3 = 0
-    sterr1, sterr2, sterr3 = 0
+    terr1 = 0
+    terr2 = 0
+    terr3 = 0
+    sterr1 = 0
+    sterr2 = 0 
+    sterr3 = 0
     for i in test:
         example = test[i]
         result = Test(w1, example)
@@ -254,7 +267,7 @@ def main():
         for l in f:
             terms = l.strip().split(',')
             attributes = [ 1, float(terms[0]), float(terms[1]), float(terms[2]), float(terms[3]) ]
-            if float(terms(4)) == 0:
+            if float(terms[4]) == 0:
                 label = -1
             else:
                 label = 1
@@ -268,7 +281,7 @@ def main():
         for l in f:
             terms = l.strip().split(',')
             attributes = [ 1, float(terms[0]), float(terms[1]), float(terms[2]), float(terms[3]) ]
-            if float(terms(4)) == 0:
+            if float(terms[4]) == 0:
                 label = -1
             else:
                 label = 1 
